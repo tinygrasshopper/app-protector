bbdd8144c5c26718c6be245e5c478e011522f9a2
Josh Hill <jhill@pivotal.io>
2016-11-28 14:16:04 +0000

    Make objects great again
    Signed-off-by: Jonathan Qiwen Tsang <jtsang@pivotal.io>

--- app-monitor/app-monitor.rb
+++ app-monitor/app-monitor.rb
@@ -3,80 +3,126 @@ require 'date'
 require 'open-uri'
 require 'httparty'
 
-class App
-  attr_reader :name, :url, :org_name, :space_name
+class Config
+  attr_reader :api_url, :username, :password, :org_name, :space_name, :space_guid
 
-  def initialize(name:, url:, org_name:, space_name:)
-    @name, @url, @org_name, @space_name = name, url, org_name, space_name
+  def initialize
+    @api_url = ENV.fetch('API_URL')
+    @username = ENV.fetch('USERNAME')
+    @password = ENV.fetch('PASSWORD')
+    @org_name = "system"
+    @space_name = "system"
+    @space_guid = ENV.fetch('SPACE_GUID')
   end
 end
 
-def login_to_cf
-  %x(./cf api #{ENV.fetch('API_URL')} --skip-ssl-validation)
-  %x(./cf auth #{ENV.fetch('USERNAME')} #{ENV.fetch('PASSWORD')})
-end
+class CFClient
+  def initialize(api_url:, username:, password:)
+    @api_url, @username, @password = api_url, username, password
+  end
 
-def target_space
-  %x(./cf target -o system -s system)
-end
+  def login
+    %x(./cf api #{@api_url} --skip-ssl-validation)
+    %x(./cf auth #{@username} #{@password})
+  end
 
-def result(timestamp)
-  path = "/v2/events?q=type:audit.app.create&q=timestamp>#{timestamp}"
-  encoded_path = URI::encode(path)
-  puts encoded_path
-  raw = %x(./cf curl \"#{encoded_path}\")
-  body = JSON.parse(raw)
-  results = body.fetch('resources')
-  filtered_results = results.select { |result| result.fetch('entity').fetch('space_guid') == ENV.fetch('SPACE_GUID') }
-end
+  def target_space(org:, space:)
+    %x(./cf target -o #{org} -s #{space})
+  end
 
-def results_since(time)
-  timestamp = time.strftime('%FT%TZ')
-  result(timestamp)
-end
+  def apps_created_since(time:, space_guid:)
+    new_app_create_events = app_create_events_since(time: time, space_guid: space_guid)
 
-def apps_from(app_results)
-  app_results.map do |app|
-    app_guid = app.fetch("entity").fetch("actee")
-    app_name = app.fetch("entity").fetch("actee_name")
-    org_name = "system"
-    space_name = "system"
-    raw_routes_results = %x(cf curl /v2/apps/#{app_guid}/routes)
+    new_app_create_events.map do |event|
+      url = app_url(event.actee)
+      App.new(
+        name: event.actee_name,
+        url: url
+      )
+    end
+  end
 
-    routes_results = JSON.parse(raw_routes_results)
-    host = routes_results.fetch("resources").first.fetch("entity").fetch("host")
-    domain_path = routes_results.fetch("resources").first.fetch("entity").fetch("domain_url")
-    raw_domain_result = %x(cf curl #{domain_path})
+  def app_create_events_since(time:, space_guid:)
+    timestamp = time.strftime('%FT%TZ')
+    path = "/v2/events?q=type:audit.app.create&q=timestamp>#{timestamp}"
+    events = curl_resources(path)
+    filtered_events = events.select { |event| event.fetch('entity').fetch('space_guid') == space_guid }
+    filtered_events.map do |event|
+      puts event
+      AppEvent.new(actee: event.fetch("entity").fetch("actee"), actee_name: event.fetch("entity").fetch("actee_name"))
+    end
+  end
 
-    domain_result = JSON.parse(raw_domain_result)
-    domain = domain_result.fetch("entity").fetch("name")
+  private
+
+  def app_url(app_guid)
+    routes_results = curl_resources("/v2/apps/#{app_guid}/routes")
+    first_route = routes_results.first
+
+    host = first_route.fetch("entity").fetch("host")
+    domain = domain_name(first_route.fetch("entity").fetch("domain_url"))
+
     url = "http://#{host}.#{domain}"
-    App.new(name: app_name, url: url, org_name: org_name, space_name: space_name)
+  end
+
+  def domain_name(domain_url)
+    domain_result = curl(domain_url)
+    domain_result.fetch("entity").fetch("name")
+  end
+
+  def curl_resources(path)
+    results = curl(path)
+    results.fetch('resources')
+  end
+
+  def curl(path)
+    encoded_path = URI::encode(path)
+    response_body = %x(./cf curl \"#{encoded_path}\")
+    JSON.parse(response_body)
   end
 end
 
-def trigger_pen_test_with app
+class App
+  attr_reader :name, :url
+
+  def initialize(name:, url:)
+    @name, @url = name, url
+  end
+end
+
+class AppEvent
+  attr_reader :actee, :actee_name
+
+  def initialize(actee:, actee_name:)
+    @actee, @actee_name = actee, actee_name
+  end
+
+  def app
+    App.new(guid: actee_name)
+  end
+end
+
+def trigger_pen_test_with app, config
   body = {
     site: app.url,
     app_name: app.name,
-    org_name: app.org_name,
-    space_name: app.space_name
+    org_name: config.org_name,
+    space_name: config.space_name
   }
   # HTTParty.post(ENV.fetch('PEN_TEST_URL'), body: body)
   puts body
 end
 
-login_to_cf
-target_space
+config = Config.new
+cf_client = CFClient.new(api_url: config.api_url, username: config.username, password: config.password)
+cf_client.login
+cf_client.target_space(org: config.org_name, space: config.space_name)
 
-last_poll_at = Time.now.utc - 300
+last_poll_at = Time.now.utc - 1000
 # loop do
-  newly_created_app_results = results_since(last_poll_at)
-  apps = apps_from(newly_created_app_results)
+  apps = cf_client.apps_created_since(time: last_poll_at, space_guid: config.space_guid)
   apps.each do |app|
-    trigger_pen_test_with(app)
+    trigger_pen_test_with(app, config)
   end
-
-
   # sleep 5
 # end
